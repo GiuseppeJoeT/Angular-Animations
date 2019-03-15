1f7cd827ebb4db9ac878aeeec53bf8b9b3a6dc71
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-03-15 11:15:56 +0100

    root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'anim-root',
   templateUrl: './app.component.html',
+  // template:`<h1 style="color:red">test html color in ts file</h1>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
